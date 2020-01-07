d5070ee375e41e38ecdc566f3e6d1e3d7addf294
Courtney Owen <cowen@microsoft.com>
2020-01-07 12:40:35 -0800

    Add support for Outlook SSO
    - Also fix minor taskpane UI issues cited in https://office.visualstudio.com/OC/_workitems/edit/3865700

--- convertToSingleHost.js
+++ convertToSingleHost.js
@@ -1,7 +1,7 @@
 const convertTest = process.argv[3] === "convert-test";
 const fs = require("fs");
 const host = process.argv[2];
-const hosts = ["excel", "powerpoint", "word"];
+const hosts = ["excel", "outlook", "powerpoint", "word"];
 const path = require("path");
 const util = require("util");
 const testPackages = ["@types/mocha", "@types/node", "current-processes", "mocha", "office-addin-test-helpers",
@@ -80,9 +80,15 @@ async function updatePackageJsonForSingleHost(host) {
 
     // remove scripts that are unrelated to the selected host
     Object.keys(content.scripts).forEach(function (key) {
-        if (key === "convert-to-single-host"
-        ) {
+        if (key === "convert-to-single-host") {
             delete content.scripts[key];
+        }
+        if (key === "sideload" && host === "outlook") {
+            delete content.scripts[key];
+        }
+
+        if (key === "start" && host === "outlook") {
+            content.scripts[key] = "npm run build:dev && concurrently \"npm run start:server\"";
         }
     });
 
--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -6,15 +6,19 @@
 /* global Office, OfficeExtension */
 
 import * as excel from "./excel";
+import * as outlook from "./outlook";
 import * as powerpoint from "./powerpoint";
 import * as word from "./word";
 
 export function writeDataToOfficeDocument(result: string[]): Promise<any> {
-  return new OfficeExtension.Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     try {
       switch (Office.context.host) {
         case Office.HostType.Excel:
           excel.writeDataToOfficeDocument(result);
+          break;
+        case Office.HostType.Outlook:
+          outlook.writeDataToOfficeDocument(result);
           break;
         case Office.HostType.PowerPoint:
           powerpoint.writeDataToOfficeDocument(result);
@@ -23,7 +27,7 @@ export function writeDataToOfficeDocument(result: string[]): Promise<any> {
           word.writeDataToOfficeDocument(result);
           break;
         default:
-          throw "Unsupported Office host application: This add-in only runs on Excel, PowerPoint, or Word.";
+          throw "Unsupported Office host application: This add-in only runs on Excel, Outlook, PowerPoint, or Word.";
       }
       resolve();
     } catch (error) {
