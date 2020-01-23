0699483e991d1d07215f5afd67ad5ac452e937eb
Courtney Owen <38081557+TCourtneyOwen@users.noreply.github.com>
2020-01-23 12:56:37 -0800

    Add outlook support (#7)
    * Add support for Outlook SSO

---
+++ src/taskpane/outlook.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global $, document, Office */
+
+import { getGraphData } from "./../helpers/ssoauthhelper";
+
+Office.onReady(info => {
+    if (info.host === Office.HostType.Outlook) {
+        $(document).ready(function () {
+            $("#getGraphDataButton").click(getGraphData);
+        });
+    }
+});
+
+export function writeDataToOfficeDocument(result: Object): void {
+    let data: string[] = [];
+    let userProfileInfo: string[] = [];
+    userProfileInfo.push(result["displayName"]);
+    userProfileInfo.push(result["jobTitle"]);
+    userProfileInfo.push(result["mail"]);
+    userProfileInfo.push(result["mobilePhone"]);
+    userProfileInfo.push(result["officeLocation"]);
+
+    for (let i = 0; i < userProfileInfo.length; i++) {
+        if (userProfileInfo[i] !== null) {
+            data.push(userProfileInfo[i]);
+        }
+    }
+
+    let userInfo: string = "";
+    for (let i = 0; i < data.length; i++) {
+        userInfo += data[i] + "\n";
+    }
+    Office.context.mailbox.item.body.setSelectedDataAsync(userInfo, { coercionType: Office.CoercionType.Html });
+}
